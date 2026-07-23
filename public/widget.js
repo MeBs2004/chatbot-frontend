6feecd10a434598752ce3f4b82bdc74f9f768c47
MeBs2004 <bharatsharma3399@gmail.com>
2026-07-23 14:49:16 +0530

    new commit

--- public/widget.js
+++ public/widget.js
@@ -74,8 +74,8 @@
   img.src = logo;
 
   Object.assign(img.style, {
-    width: imgSize + "70px",
-    height: imgSize + "70px",
+    width: imgSize + "px",
+    height: imgSize + "px",
     objectFit: isOya ? "cover" : "contain",
     borderRadius: isOya ? "50%" : "0",
     display: "block",
